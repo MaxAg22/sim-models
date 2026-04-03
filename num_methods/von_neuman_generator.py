ccdefe24cca8474bfdf72c8baf6b5dc2bd7f64d4
Max <maximo.aguero@mi.unc.edu.ar>
2026-04-03 09:54:59 -0300

    periodo para von neuman

--- num_methods/von_neuman_generator.py
+++ num_methods/von_neuman_generator.py
@@ -6,10 +6,19 @@ def von_neumann(d):
 
 u = int(input("4 digits seed: "))
 n = int(input("sequence length: "))
+k = 0
+pos = 0
+period = False
 result = []
 
 for _ in range(n):
     u = von_neumann(u)
+    if not period and u in result:
+        pos = result.index(u)
+        k = len(result) - pos
+        period = True
+
     result.append(u)
 
 print(result)
+print(f"period: {k} and n0: {pos+1}" )
