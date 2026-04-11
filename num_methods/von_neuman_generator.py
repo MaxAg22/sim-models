393eccd5878f7c4028c21bd671771dfc2a21c50f
Max <maximo.aguero@mi.unc.edu.ar>
2026-04-10 23:23:05 -0300

    refactor deaa

--- num_methods/von_neuman_generator.py
+++ num_methods/von_neuman_generator.py
@@ -1,24 +1,31 @@
 """module"""
+
+
 def von_neumann(d):
     """von Neuman generator"""
     d = (d**2 // 100) % 10000
     return d
 
-u = int(input("4 digits seed: "))
-n = int(input("sequence length: "))
-k = 0
-pos = 0
-period = False
-result = [u]
+def get_von_neuman_seq(u,n_sim):
+    """generates a von neuman seq"""
+    k = 0
+    pos = 0
+    period = False
+    seq = [u]
 
-for _ in range(n):
-    u = von_neumann(u)
-    if not period and u in result:
-        pos = result.index(u)
-        k = len(result) - pos
-        period = True
+    for _ in range(n_sim):
+        u = von_neumann(u)
+        if not period and u in seq:
+            pos = seq.index(u)
+            k = len(seq) - pos
+            period = True
+        seq.append(u)
 
-    result.append(u)
+    return (seq, k, pos)
 
-print(result)
-print(f"period: {k} and n0: {pos}" )
+
+if __name__ == "__main__":
+    seed = int(input("4 digits seed: "))
+    n = int(input("sequence length: "))
+    result = get_von_neuman_seq(seed, n)
+    print(f"Sequence: {result[0]} \nK: {result[1]}\nN0: {result[2]}")
