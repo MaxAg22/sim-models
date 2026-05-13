11b68b0575433d97141f0dbe42b95893e6ab60eb
Max <maximo.aguero@mi.unc.edu.ar>
2026-05-13 18:37:41 -0300

    ej 4 parcial 2022

--- practice/exam/parcial2024/ej4.py
+++ practice/exam/parcial2024/ej4.py
@@ -1,4 +1,5 @@
 from random import random
+from math import log
 
 
 def tirar_moneda(p):
@@ -30,3 +31,39 @@ def estimar_prob(x, p, n_sim):
 
 
 print(f"P(X = 4) ~ {estimar_prob(4, 1/3, 10000):.4f}")
+
+
+# ej 4b)
+
+def geom(p):
+    u = random()
+    return int(log(1-u)/log(1-p)) + 1  # o aca poner + 2
+
+
+def p(j):
+    if j < 2:
+        return 0
+    return ((2 ** (j-1)) + 2) / (3 ** j)
+
+
+def q(j): return (1/3) * ((2/3) ** (j - 1))
+
+
+def generar_x(p_geom):
+    while True:
+        y = geom(p_geom)
+        u = random()
+        if u < p(y) / (q(y) * 2):
+            return y
+
+
+def estimar_con_aceptacion_rechazo(p_geom, n_sim):
+    count = 0
+    for _ in range(n_sim):
+        x = generar_x(p_geom)
+        if x == 4:
+            count += 1
+    return count / n_sim
+
+
+print(f"AyR: P(X = 4) ~ {estimar_con_aceptacion_rechazo(1/3, 10000):.4f}")
