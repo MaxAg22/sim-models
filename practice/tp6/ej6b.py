d15aac027a7445914ace2978d1f04a0022c44d71
Maxi <maximo.aguero.p3@gmail.com>
2026-06-02 10:59:09 -0300

    ej7a) perfecto

--- practice/tp6/ej6b.py
+++ practice/tp6/ej6b.py
@@ -16,26 +16,26 @@ def calcular_media(datos):
 def calcular_varianza_muestral(datos):
     """Calcula el estadístico S^2."""
     media = calcular_media(datos)
-    return sum((x - media)**2 for x in datos) / (len(datos) - 1)
+    return sum((x - media) ** 2 for x in datos) / (len(datos) - 1)
 
 
-def ejercicio_6a_ideal(N):
-    datos_originales = [1, 3]
+def ejercicio_6b(N):
+    datos_originales = [5, 4, 9, 6, 21, 17, 11, 20, 7, 10, 21, 15, 13, 16, 8]
     n = len(datos_originales)
 
     # 2. Generar todas las N muestras bootstrap
     muestras_bootstrap = calular_muestras_bootstrap(datos_originales, N)
 
     # 3. Calcular S^2 para cada muestra bootstrap
-    replicaciones_s2 = [calcular_varianza_muestral(
-        m) for m in muestras_bootstrap]
+    replicaciones_s2 = [calcular_varianza_muestral(m) for m in muestras_bootstrap]
 
     # 4. Calcular el promedio de las replicaciones
     media_de_replicaciones = calcular_media(replicaciones_s2)
 
     # 5. Calcular la Varianza Bootstrap del estimador
     suma_errores_cuadrados = sum(
-        (s2 - media_de_replicaciones)**2 for s2 in replicaciones_s2)
+        (s2 - media_de_replicaciones) ** 2 for s2 in replicaciones_s2
+    )
 
     varianza_bootstrap_final = suma_errores_cuadrados / (N - 1)
 
@@ -43,4 +43,4 @@ def ejercicio_6a_ideal(N):
 
 
 # Resultado esperado: 1.0
-print(f"La estimación bootstrap de Var(S^2) es: {ejercicio_6a_ideal(10000)}")
+print(f"La estimación bootstrap de Var(S^2) es: {ejercicio_6b(10000)}")
--- practice/tp6/ej7.py
+++ practice/tp6/ej7.py
@@ -1,117 +1,103 @@
-from collections import deque
+from math import log
+from random import random
 
 
-# t tiempo simulado transcurrido
-# n_solicitudes número de solicitudes en el instante t
-# solicitudes: número de solicitudes hasta el tiempo t
-# servicios_atendidos: número de servicios atendidos hasta el tiempo t
-# descansos[]: lista de descansos, en donde un descanso es de la forma (t_inicio, t_final)
-# servicios_pendientes[]: cola de servicios por atender
-
-def generar_llegada():
-    return 10
+def lambda_t(t):
+    """Función de intensidad lambda(t)"""
+    t_aux = t % 10
+    if 0 <= t_aux <= 5:
+        return 3 * t_aux + 4
+    elif 5 < t_aux <= 10:
+        return 34 - 3 * t_aux
 
 
-def generar_atencion():
-    return 10
+def generar_proximo_arribo(t_actual, lamda_max=19, t_max=100):
+    """
+    RetorNa el tiempo (t) del PRÓXIMO arribo
+    basado en el tiempo actual de la simulación.
+    """
+    t = t_actual
+    while True:
+        u = 1 - random()
+        t += -log(u) / lamda_max  # Avanzamos el reloj tentativo
+        if t > t_max:  # Límite de la simulación (T)
+            return float("inf")
+
+        v = random()
+        if v < lambda_t(t) / lamda_max:  # Criterio de adelgazamiento [2]
+            return t  # Aceptamos este tiempo como el próximo arribo
 
 
-def generar_descanso():
-    return 10
+def generar_atencion(lamda=13):
+    """Generamos uNa exponencial con tasa lamda"""
+    u = 1 - random()
+    return -log(u) / lamda
 
 
 def ejercicio_7a(tiempo_total):
     t = 0
-    n_solicitudes_actuales = 0  # en instante t
+    n = 0  # En instante t
 
     # Solicitudes
-    cantidad_solicitudes = 0
-    llegada_de_solicitudes = []
-    cantidad_solicitudes_atendidas = 0
-    tiempo_atencion_de_solicitudes = []
-
-    # Descansos
-    fin_descanso = tiempo_total
-    descansando = False
-    descansos = []
-    servicios_pendientes = deque()
+    Na = 0
+    tiempos_Na = []
+    Nd = 0
+    tiempos_Nd = []
 
     # Tiempos
-    tiempo_llegada = generar_llegada()
-    tiempo_proxima_llegada = tiempo_llegada
-    tiempo_fin_servicio = tiempo_total  # "infinito"
+    T0 = generar_proximo_arribo(t)
+    Ta = T0
+    Td = float("inf")
 
     while t < tiempo_total:
-        proximo_evento = min(tiempo_proxima_llegada,
-                             tiempo_fin_servicio, fin_descanso)
+        proximo_evento = min(Ta, Td)
 
-        # caso 1: Llego una solicitud
-        if proximo_evento == tiempo_proxima_llegada:
-            t = tiempo_proxima_llegada
-            n_solicitudes_actuales += 1
-            servicios_pendientes.append((t, n_solicitudes_actuales))
-            cantidad_solicitudes += 1
-            tiempo_prox_llegada = generar_llegada()
-            tiempo_proxima_llegada = t + tiempo_prox_llegada
+        # Caso 1: Llego uNa solicitud
+        if proximo_evento == Ta:
+            t = Ta
+            n += 1
+            Na += 1
+            tiempo_prox_llegada = generar_proximo_arribo(t)
+            Ta = tiempo_prox_llegada
 
-            # si se pasa
-            if tiempo_proxima_llegada > tiempo_total:
-                tiempo_proxima_llegada = tiempo_total
+            # Si se pasa
+            if Ta > tiempo_total:
+                Ta = float("inf")
 
-            # tenemos que atender la primer solicitud
-            if n_solicitudes_actuales == 1 and not descansando:
+            # Tenemos que ateNder la primer solicitud
+            if n == 1:
                 tiempo_atencion = generar_atencion()
-                tiempo_fin_servicio = t + tiempo_atencion
+                Td = t + tiempo_atencion
 
-            # registrar
-            llegada_de_solicitudes.append(t)
+            # Registrar
+            tiempos_Na.append(t)
 
-        # caso 2: Debemos atender una solicitud
-        elif proximo_evento == tiempo_fin_servicio:
-            t = tiempo_fin_servicio
-            cantidad_solicitudes_atendidas += 1
-            n_solicitudes_actuales -= 1
-            servicios_pendientes.append((t, n_solicitudes_actuales))
+        # Caso 2: Debemos ateNder uNa solicitud
+        elif proximo_evento == Td:
+            t = Td
+            Nd += 1
+            n -= 1
 
-            # registrar
-            tiempo_atencion_de_solicitudes.append(t)
-
-            if n_solicitudes_actuales == 0:
-                tiempo_fin_servicio = tiempo_total
-                fin_descanso = t + generar_descanso()
-                descansando = True
-                descansos.append((t, fin_descanso - t))
-
-                # El descanso supera el tiempo que el servidor atiende
-                if fin_descanso > tiempo_total:
-                    fin_descanso = tiempo_total
-
+            if n == 0:
+                Td = float("inf")
             else:
                 tiempo_atencion = generar_atencion()
-                tiempo_fin_servicio = t + tiempo_atencion
+                Td = t + tiempo_atencion
 
-        # gestionar el descanso, re-descanso si no hay solicitudes
-        elif proximo_evento == fin_descanso:
-            t = fin_descanso
-            if n_solicitudes_actuales == 0:
-                fin_descanso = t + generar_descanso()
-                descansando = True
-                descansos.append((t, fin_descanso - t))
-
-                # El descanso supera el tiempo que el servidor atiende
-                if fin_descanso > tiempo_total:
-                    fin_descanso = tiempo_total
-            else:
-                descansando = False
-                fin_descanso = tiempo_total
-                tiempo_atencion = generar_atencion()
-                tiempo_fin_servicio = t + tiempo_atencion
+            # Registrar
+            tiempos_Nd.append(t)
 
     return (
-        cantidad_solicitudes,
-        llegada_de_solicitudes,
-        cantidad_solicitudes_atendidas,
-        tiempo_atencion_de_solicitudes,
-        descansos,
-        servicios_pendientes
+        Na,
+        tiempos_Na,
+        Nd,
+        tiempos_Nd,
     )
+
+
+res = ejercicio_7a(100)
+print("\n*** Ejercicio 7 ***")
+print(f"Cantidad de solicitudes: {res[0]}")
+print(f"Tiempo de llegada de solicitudes: {res[1]}")
+print(f"Cantidad de solicitudes ateNdidas: {res[2]}")
+print(f"Tiempo de atención de solicitudes: {res[3]}")
